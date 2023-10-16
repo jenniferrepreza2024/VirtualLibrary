ecee7d8d1b2a6df4e46c7a9d441db4b3940d039a
jenniferrepreza2024 <jenniferrepreza2024>
2023-10-16 08:26:16 -0600

    proyecto

--- src/main.js
+++ src/main.js
@@ -1,9 +1,35 @@
 // Datos de ejemplo (puedes reemplazarlos con tus propios datos)
+    
 const books = [
-    { title: "Book 1", author: "Author 1" },
-    { title: "Book 2", author: "Author 2" },
-    { title: "Book 3", author: "Author 3" },
-    // Agrega más libros aquí
+    { title:"El perfume del Rey", author: "Karine Bernal Lobo", img:"../img/libro1.JPG"   },
+    { title: "1984", author: "Georgel Orwell", img:"../img/libro2.JPG" },
+    { title: "Historia del tiempo", author: "Stephen W.Hawking", img:"../img/libro3.JPG" },
+    { title: "12 reglas para vivir", author: "Jordan B.Peterson", img:"../img/libro4.JPG" },
+    { title: "Purple Hearths", author: "Tess Wakefield", img:"../img/libro5.JPG" },
+    { title: "Ignite me", author: "Tahereh Mafi", img:"../img/libro6.JPG" },
+    { title: "Shadow me", author: "Tahereh Mafi", img:"../img/libro7.JPG" },
+    { title: "El dia que dejo de nevar en Alaska", author: "Alice Kellen", img:"../img/libro8.JPG" },
+    { title: "Nosotros en la Luna", author: "Alice Kellen", img:"../img/libro9.JPG" },
+    { title: "Las Alas de Sophie", author: "Alice Kellen", img:"../img/libro10.JPG" },
+    { title: "El Héroe perdido", author: "Rick Riordan", img:"../img/libro11.JPG" },
+    { title: "El hijo de Neptuno", author: "Rick Riordan", img:"../img/libro12.JPG" },
+    { title: "La casa de Hades", author: "Rick Riordan", img:"../img/libro13.JPG" },
+    { title: "El Ladrón del Rayo", author: "Rick Riordan", img:"../img/libro14.JPG" },
+    { title: "La batalla del Laberinto", author: "Rick Riordan", img:"../img/libro15.JPG" },
+    { title: "La pirámide roja", author: "Rick Riordan", img:"../img/libro16.JPG" },
+    { title: "El trono de fuego", author: "Rick Riordan", img:"../img/libro17.JPG" },
+    { title: "Harry Potter y la piedra filosofal", author: "J.K Rowling", img:"../img/libro18.JPG" },
+    { title: "Harry Potter y las reliquias de la Muerte", author: "J.K Rowling", img:"../img/libro19.JPG" },
+    { title: "Los Crímenes de Grindelwald", author: "J.K Rowling", img:"../img/libro20.JPG" },
+    { title: "Animales Fántasticos y donde encontrarlos", author: "J.K Rowling", img:"../img/libro21.JPG" },
+    { title: "Los cuentos de Beedle el Bardo", author: "J.K Rowling", img:"../img/libro22.JPG" },
+    { title: "Todo lo que nunca fuimos", author: "Alice Kellen", img:"../img/libro23.JPG" },
+    { title: "Todo lo que somos juntos", author: "Alice Kellen", img:"../img/libro24.JPG" },
+    { title: "El chico que dibujaba constelaciones", author: "Alice Kellen", img:"../img/libro25.JPG" },
+    { title: "Tú y yo invencibles", author: "Alice Kellen", img:"../img/libro26.JPG" },
+    { title: "El lenguaje de las espinas", author: "Leihg Bardugo", img:"../img/libro27.JPG" },
+    { title: "La marca de Atenea", author: "Rick Riordan", img:"../img/libro28.JPG" },
+    { title: "La sangre del Olimpo", author: "Rick Riordan", img:"../img/libro29.JPG" },
 ];
 
 const searchInput = document.getElementById("searchInput");
@@ -18,7 +44,8 @@ function displaySearchResults(results) {
     // Mostrar los resultados
     results.forEach(book => {
         const li = document.createElement("li");
-        li.textContent = `${book.title} by ${book.author}`;
+        //li.textContent = ` `;
+        li.innerHTML = `<p> ${book.title} by ${book.author} </p> <img src="${book.img }"  /> `;
         bookList.appendChild(li);
     });
 }
